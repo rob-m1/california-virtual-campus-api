2f6de5136837adee49eb9f54ea734625be775dd7
Robert Meli <robertseveromeli@berkeley.edu>
2025-04-10 02:50:28 -0700

    Setup Changes

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+#with open('./requirements.txt') as f:
+#    requirements = f.read().splitlines()
 
 classifiers = [
   'Development Status :: 3 - Alpha',
@@ -11,18 +11,20 @@ classifiers = [
  
 setup(
   name='CaliforniaVirtualCampusAPI',
-  version='0.0.1',
+  version='0.0.5',
   description='A simple API for the California Virtual Campus platform.',
   long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(),
   url='https://github.com/rob-m1/california-virtual-campus-api',  
   author='Robert Meli',
   author_email='robertmeli1@outlook.com',
-  license='APACHE', 
   classifiers=classifiers,
-  keywords='california virtual campus', 
+  keywords=['california virtual campus','web scraping', 'beautifulsoup', 'api', 'education', 'course scraping', 'student resources', 'college resources'], 
   package_dir={'': 'src'},
   packages=find_packages(where='src'),
-  install_requires=requirements, 
+  install_requires=[
+    "requests",
+    "beautifulsoup4",
+], 
   project_urls={
         'Issue Tracker': 'https://github.com/rob-m1/california-virtual-campus-api/issues',
     }
